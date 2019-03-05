928c7c53ea82ce5c4cac7b68482e3cde8ce75353
Alejandro <alejamp@gmail.com>
2019-03-05 13:02:25 -0800

    fix dependencies, update dart sdk env version

--- lib/src/damerau.dart
+++ lib/src/damerau.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, Kwang Yul Seo. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:quiver_iterables/iterables.dart';
+import 'package:quiver/iterables.dart';
 
 import './base.dart';
 
@@ -74,4 +74,4 @@ class Damerau implements StringDistance {
 
     return h[s1.length + 1][s2.length + 1];
   }
-}
+} 
